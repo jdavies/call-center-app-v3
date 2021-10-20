6a5bd83e23271cea69d32a4a9d9b016107dd1c80
Jeff Davies <297151+jdavies@users.noreply.github.com>
2021-10-20 13:45:04 -0700

    Got the crypto-js library working

--- api-server/google/login/ReadMe.md
+++ api-server/google/login/ReadMe.md
@@ -8,3 +8,23 @@ To test the login function, use th following payload n the body of the test call
     "password": "demo"
 }
 ```
+
+Deploy the function to the cloud:
+
+```sh
+gcloud functions deploy login --runtime=nodejs14 --trigger-http \
+  --region=us-west2 --allow-unauthenticated --memory=128MB \
+  --max-instances=10
+```
+
+You can describe the function with this command:
+
+```sh
+gcloud functions describe login
+```
+
+You can delete the function with this command:
+
+```sh
+gcloud functions delete login
+```
--- api-server/google/login/index.js
+++ api-server/google/login/index.js
@@ -1,6 +1,6 @@
 const escapeHtml = require('escape-html');
-const crypto = require('crypto');
-const hash = crypto.createHash('sha256');
+const crypto = require('crypto-js');
+var SHA256 = require("crypto-js/sha256");
 
 /**
  * Responds to any HTTP request.
@@ -9,25 +9,14 @@ const hash = crypto.createHash('sha256');
  * @param {!express:Response} res HTTP response context.
  */
 exports.login = (req, res) => {
-    // Is the Crypto package available?
-    let crypto;
-    try {
-        crypto = await import('crypto');
-
-        // Yes,crypto is available and ready
-        console.log('crypto support is ENABLED!');
-
-    } catch (err) {
-        console.log('crypto support is disabled!');
-    }
     // Logging in must be done via POST
     if(req.method == 'POST') {
         // First we extract the provided username and password
         let username = req.body.username;
         let password = req.body.password;
 
-        // SHA256 encruypt the plain-text password
-        password = crypto.createHash("sha256").update(password).digest("hex");
+        // SHA256 encrypt the plain-text password
+        password = SHA256(password);
 
         console.log('password encrypted to ' + password);
         // Now call Astra to ensure this record exists. If it does, the call will return a UUID
