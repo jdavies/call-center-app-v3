532f4cab6ad477a22a2b3ec28788d9b45114d58b
Jeff Davies <297151+jdavies@users.noreply.github.com>
2021-10-21 14:39:42 -0700

    utiity file to expedite the build process.

---
+++ api-server/google/login/build.sh
@@ -0,0 +1,12 @@
+date
+gcloud functions deploy login --runtime=nodejs14 --trigger-http \
+  --region=us-west2 --allow-unauthenticated --memory=128MB \
+  --set-env-vars ASTRA_DB_ID=$ASTRA_DB_ID \
+  --set-env-vars ASTRA_DB_TOKEN=$ASTRA_DB_TOKEN \
+  --set-env-vars KEYSPACE=$KEYSPACE \
+  --set-env-vars ASTRA_DB_REGION=$ASTRA_DB_REGION \
+  --set-env-vars ASTRA_CLIENT_ID=$ASTRA_CLIENT_ID \
+  --set-env-vars ASTRA_CLIENT_SECRET=$ASTRA_CLIENT_SECRET \
+  --max-instances=10
+date
+
